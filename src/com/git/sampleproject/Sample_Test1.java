8138f665438322a754f506631b7152598a6d7cbe
imran0801 <imu.imran123@gmail.com>
2018-05-07 09:03:17 +0530

    This is my first code committing on this project

---
+++ src/com/git/sampleproject/Sample_Test1.java
@@ -0,0 +1,10 @@
+package com.git.sampleproject;
+
+public class Sample_Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is my first Git Program to be pushed");
+
+	}
+
+}
